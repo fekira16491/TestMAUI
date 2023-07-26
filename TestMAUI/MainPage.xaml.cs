b0fc304098b4414a5fd3caebdda04d904634cd48
mahalakshmi siva <mahalakshmisiva@mahalakshmisMBP.localdomain>
2023-07-26 06:16:56 +0200

    committed

--- TestMAUI/MainPage.xaml.cs
+++ TestMAUI/MainPage.xaml.cs
@@ -16,7 +16,6 @@ public partial class MainPage : ContentPage
 			CounterBtn.Text = $"Clicked {count} time";
 		else
 			CounterBtn.Text = $"Clicked {count} times";
-
 		SemanticScreenReader.Announce(CounterBtn.Text);
 
   
